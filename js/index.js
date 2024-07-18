9217f50b31c08b9f1c4019905dea0949eb221343
albert hernandez <albertjose22x@gmail.com>
2024-07-18 16:58:56 -0300

    agregado de carrito

--- js/index.js
+++ js/index.js
@@ -1,40 +1,6 @@
-const CAROUSEL_ENTRADA = document.querySelector(".carousel-entrada");
-const CAROUSEL_ITEM = document.querySelectorAll(".carousel-item");
-const CAROUSEL_PREV = document.getElementById("carousel-prev");
-const CAROUSEL_NEXT = document.getElementById("carousel-next");
+const btnCart = document.querySelector('.container-icon')
+const containerCartProducts = document.querySelector('.container-cart-products')
 
-let contador = 0;
-
-console.log(CAROUSEL_ITEM)
-CAROUSEL_PREV.addEventListener("click", ()=>{
-    contador--;
-    if(contador < 0){
-        contador = CAROUSEL_ITEM.length - 1;
-    }
-    carouselActualizacion();
-
+btnCart.addEventListener('click', () => {
+    containerCartProducts.classList.toggle('hidden-cart')
 })
-
-CAROUSEL_NEXT.addEventListener("click", ()=>{
-    contador++;
-    if(contador >= CAROUSEL_ITEM.length){
-        contador = 0;
-    }
-    carouselActualizacion();
-    
-});
-
-function carouselActualizacion(){
-    CAROUSEL_ITEM.forEach((item, index)=>{
-        item.classList.remove('active');
-        if(index === contador){
-            item.classList.add('active');
-        }
-    });
-}
-
-carouselActualizacion();
-
-document.getElementById('buy-button').addEventListener('click', function() {
-    alert('Producto añadido al carrito de compras.');
-});
